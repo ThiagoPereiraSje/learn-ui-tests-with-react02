b0ae6e2e259447bc1b439862b270ccab1e0fac09
Thiago Pereira <thiagopereiramail@gmail.com>
2022-03-14 15:15:28 +0000

    understand the syntax of test block

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+// Syntax of Test Block
 test('renders learn react link', () => {
+  // First step: Render component in virtual DOM
   render(<App />);
+
+  // Second step: Find elements we want to interact with
   const linkElement = screen.getByText(/learn react/i);
+
+  // Third step: Interact with those elements
+
+  // Fourth step: Assert that the results are expected
   expect(linkElement).toBeInTheDocument();
 });
