33c182560cd53a1b7064ef5bdcb84848f8158d12
Thiago Pereira <thiagopereiramail@gmail.com>
2022-03-17 19:32:31 +0000

    Query methods

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,17 @@
-import React from 'react'
-import "./Header.css"
+import React from "react";
+import "./Header.css";
 
-export default function Header({
-    title
-}) {
-    return <h1 className="header">{title}</h1>
+export default function Header({ title }) {
+  return (
+    <>
+      <h1 className="header">{title}</h1>
+      <h3 className="header">Cats</h3>
+      <h3 title="Header" className="header">
+        Dogs
+      </h3>
+      <h3 data-testid="header-1" className="header">
+        Birds
+      </h3>
+    </>
+  );
 }
---
+++ src/components/Header/__test__/Header.test.js
@@ -0,0 +1,78 @@
+import { render, screen } from "@testing-library/react";
+import Header from "../Header";
+
+test("Get element by text", () => {
+  // Render component in virtual DOM
+  render(<Header title="My Header" />);
+
+  // Find elements we want to interact with
+  const headingElement = screen.getByText(/my header/i);
+  expect(headingElement).toBeInTheDocument();
+});
+
+// test("Get element by role", () => {
+//   // Render component in virtual DOM
+//   render(<Header title="My Header" />);
+
+//   // Find elements we want to interact with
+//   const headingElement = screen.getByRole("heading");
+//   expect(headingElement).toBeInTheDocument();
+// });
+
+test("Get many elements by role", () => {
+  // Render component in virtual DOM
+  render(<Header title="My Header" />);
+
+  // Find elements we want to interact with
+  const headingElements = screen.getAllByRole("heading");
+  expect(headingElements.length).toBe(4);
+});
+
+test("Get element by role and text", () => {
+  // Render component in virtual DOM
+  render(<Header title="My Header" />);
+
+  // Find elements we want to interact with
+  const headingElement = screen.getByRole("heading", { name: "My Header" });
+  expect(headingElement).toBeInTheDocument();
+});
+
+test("Get element by title", () => {
+  // Render component in virtual DOM
+  render(<Header title="My Header" />);
+
+  // Find elements we want to interact with
+  const headingElement = screen.getByTitle("Header");
+  expect(headingElement).toBeInTheDocument();
+});
+
+test("Get element by testId", () => {
+  // Render component in virtual DOM
+  render(<Header title="My Header" />);
+
+  // Find elements we want to interact with
+  const headingElement = screen.getByTestId("header-1");
+  expect(headingElement).toBeInTheDocument();
+});
+
+// Find Method
+// -----------------------------------------------------------
+test("Find element by text", async () => {
+  // Render component in virtual DOM
+  render(<Header title="My Header" />);
+
+  // Find elements we want to interact with
+  const headingElement = await screen.findByText(/my header/i);
+  expect(headingElement).toBeInTheDocument();
+});
+
+// Query Method
+// -----------------------------------------------------------
+test("Query element by text", () => {
+  // Render component in virtual DOM
+  render(<Header title="My Header" />);
+
+  // Find elements we want to interact with
+  const headingElement = screen.queryByText(/snakes/i);
+  expect(headingElement).not.toBeInTheDocument();
+});
