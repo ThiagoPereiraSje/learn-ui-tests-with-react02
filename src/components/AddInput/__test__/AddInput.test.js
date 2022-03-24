eb08f6abb197560afddc885eb80ee98f6a708618
Thiago Pereira <thiagopereiramail@gmail.com>
2022-03-24 12:26:39 +0000

    Test the component events

---
+++ src/components/AddInput/__test__/AddInput.test.js
@@ -0,0 +1,56 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import AddInput from "../AddInput";
+
+// setTodos could be: () => {}. It's completely valid
+// Better approach: mockSetTodo
+const mockedSetTodo = jest.fn();
+
+describe("AddInput", () => {
+  test("Should render input element", () => {
+    // Render component in virtual DOM
+    render(<AddInput todos={[]} setTodos={mockedSetTodo} />);
+
+    // Find elements we want to interact with
+    const inputElement = screen.getByPlaceholderText(/add a new task here/i);
+
+    // Interact with those elements
+
+    // Assert that the results are as expected
+    expect(inputElement).toBeInTheDocument();
+  });
+
+  test("Should be able to type in input", () => {
+    // Render component in virtual DOM
+    render(<AddInput todos={[]} setTodos={mockedSetTodo} />);
+
+    // Find elements we want to interact with
+    const inputElement = screen.getByPlaceholderText(/add a new task here/i);
+
+    // Interact with those elements
+    fireEvent.change(inputElement, {
+      target: { value: "Go Grocery Shopping" },
+    });
+
+    // Assert that the results are as expected
+    expect(inputElement.value).toBe("Go Grocery Shopping");
+  });
+
+  test("Should have empty input text when add button is clicked", () => {
+    // Render component in virtual DOM
+    render(<AddInput todos={[]} setTodos={mockedSetTodo} />);
+
+    // Find elements we want to interact with
+    const inputElement = screen.getByPlaceholderText(/add a new task here/i);
+    const buttonElement = screen.getByRole("button", { name: /add/i });
+
+    // Interact with those elements
+    fireEvent.change(inputElement, {
+      target: { value: "Go Grocery Shopping" },
+    });
+
+    fireEvent.click(buttonElement);
+
+    // Assert that the results are as expected
+    expect(inputElement.value).toBe("");
+  });
+});
